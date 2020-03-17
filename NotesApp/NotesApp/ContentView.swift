5dd7a0b12fcb423a00e4aa721e9b948736166776
Anastasia <traverse@ucu.edu.ua>
2020-03-17 13:37:25 +0200

    init prj

---
+++ NotesApp/NotesApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NotesApp
+//
+//  Created by Анастасия Траверсе on 17.03.2020.
+//  Copyright © 2020 Анастасия Траверсе. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
